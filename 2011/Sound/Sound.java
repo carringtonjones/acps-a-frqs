f507c75e166b8fb0fcefcc6cb73761625d675fa0
cartras <cjones6096@greenvilleschools.us>
2017-12-14 09:35:30 -0500

    Finished

--- 2011/Sound/Sound.java
+++ 2011/Sound/Sound.java
@@ -1,1 +1,101 @@
- 
+ /**
+ * 
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Arrays;
+
+public class Sound
+{
+    /**
+     * 
+     */
+    private int[] samples;
+
+    /**
+     * Constructor for objects of Sound class
+     * The array of values in this sound; guaranteed not to be null 
+     * 
+     * @param samples Samples in an array 
+     */
+    
+    public Sound(int[] samples)
+    {
+        this.samples = samples;
+        
+    }
+
+    /** 
+     * Changes those values in this sound that have an amplitude greater than limit
+     * Values greater than limit are changed to limit
+     * Values less than -limit are changed to -limit
+     *
+     * @param limit the amplitude limit
+     * @return the number of values in this sound that the message has changed
+     */
+    
+    public int limitAmplitude(int limit)
+    {
+        
+        int count = 0;
+        
+        for (int i = 0; i < samples.length; i = i + 1) {
+            
+            if (samples[i] > limit) {
+                
+                samples[i] = limit;
+                count++;
+                
+            }
+            else if (samples[i] < -limit) {
+                
+                samples[i] = -limit;
+                count++;
+                
+            }
+            
+        }
+        
+        return count;
+        
+    }
+    
+    /** 
+     *  Removes silence from beginning of sound
+     * 0 is the value of silence 
+     *  
+     */
+    
+    public void trimSilenceFromBeginning() {
+        int count = 0;
+        
+        while (samples[count] == 0) {
+            
+            count++;
+            
+        }
+        
+        int[] result = new int[samples.length-count];
+        
+        for (int i = count; i < samples.length; i = i + 1) {
+            
+            result[i-count] = samples[i];
+            
+        }
+        
+        samples = result;
+    }
+    
+    /** 
+     *  Outputs an array of trimSilence 
+     *  @return Output of the samples in an array 
+     */
+    
+    public String toString() {
+        
+        trimSilenceFromBeginning();
+        return Arrays.toString(samples);
+        
+    }
+}
--- 2011/Sound/SoundTester.java
+++ 2011/Sound/SoundTester.java
@@ -3,20 +3,17 @@
 public class SoundTester
 {
     public static void main(String[] args) {
-        Sound sound1 = new Sound(new int[] {40, 2532, 17, -2300, -17, -4000, 2000, 1048, -420, 33, 15, -32, 2030, 3223 });
-        Sound sound2 = new Sound(new int[] {0, 0, 0, 0, -14, 0, -35, -39, 0, -7, 16, 32, 37, 29, 0, 0 });
+       
+        int[] samples1 = {40, 2532, 17, -2300, -17, -4000, 2000, 1048, -420, 33, 15, -32, 2030, 3223};
+        Sound r1 = new Sound(samples1);
+        //after trimSilenceFromBeginning has been called, the instance variable samples will refer to 
+        //the following array
+        int[] samples2 = {0, 0, 0, 0, -14, 0, -35, -39, 0, -7, 16, 32, 37, 29, 0, 0};
+        Sound r2 = new Sound(samples2);
         
-        System.out.println("Sound 1:");
-        System.out.println(sound1);
-        sound1.limitAmplitude(2000);
-        System.out.println(sound1);
-        
-        System.out.println();
-        
-        System.out.println("Sound 2:");
-        System.out.println(sound2);
-        sound2.trimSilenceFromBeginning(); 
-        System.out.println(sound2);
-    
+        System.out.println(r1.limitAmplitude(2000));
+        System.out.println(r1.toString());
+        System.out.println(r2.limitAmplitude(2000));
+        System.out.println(r2.toString());
     }
 }
