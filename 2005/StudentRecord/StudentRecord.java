078489e84569951262085abe7b4d0b7e994e5383
cartras <cjones6096@greenvilleschools.us>
2017-12-14 09:08:48 -0500

    Finished StudentRecord

--- 2005/StudentRecord/StudentRecord.java
+++ 2005/StudentRecord/StudentRecord.java
@@ -1,18 +1,35 @@
- 
+/**
+ * Constructor class for StudentRecord(s)
+ * 
+ * @author Carrington Jones
+ * @version 11/14/17
+ */ 
 
 public class StudentRecord {
     private int[] scores;
     
+    /**
+     * Constructor for scores 
+     * 
+     * @param scores Tests scores in an array 
+     */
     public StudentRecord(int[] scores) {
         this.scores = scores;
         
     }
 
+    /**
+     * Calculates the average of the test scores
+     * 
+     * @param first First set of scores in the array 
+     * @param last Last position in the array
+     * @return The average of all the scores
+     */
     private double average(int first, int last) {
         double numScores = last - first + 1;
         int sum = 0;
         
-        for (int i=first; i<=last; i = i + 1) {
+        for (int i = first; i <= last; i = i + 1) {
             sum += scores[i];
             
         }
@@ -20,8 +37,13 @@ public class StudentRecord {
         
     }
    
+    /**
+     * Determines if the student scores improvede of not
+     * 
+     * @return If student has improved or not improved
+     */
     private boolean hasImproved() {
-        for (int i=0; i<scores.length - 1; i = i + 1) {
+        for (int i = 0; i<scores.length - 1; i = i + 1) {
             if (scores[i+1] < scores[i]) {
                 return false;
                 
@@ -31,6 +53,11 @@ public class StudentRecord {
         
     }
 
+    /**
+     * If student has improved it will average the test score5
+     * 
+     * @return Average of test scores based on the improvement of each score 
+     */
     public double finalAverage() {
         if (this.hasImproved()) { 
             return average(scores.length / 2, scores.length - 1);
--- 2005/StudentRecord/StudentTester.java
+++ 2005/StudentRecord/StudentTester.java
@@ -4,13 +4,17 @@ public class StudentTester{
     
     public static void main(String[] args) {
         
-        StudentRecord student1 = new StudentRecord(new int[] {50, 50, 20, 80, 53});
-        StudentRecord student2 = new StudentRecord(new int[] {20, 50, 50, 53, 80});
-        StudentRecord student3 = new StudentRecord(new int[] {20, 50, 50, 80});
+        int[] scores1 = {50, 50, 20, 80, 53};
+        StudentRecord record1 = new StudentRecord(scores1);
         
-        System.out.printf("Student 1: %f%n", student1.finalAverage());
-        System.out.printf("Student 2: %f%n", student2.finalAverage());
-        System.out.printf("Student 3: %f%n", student3.finalAverage());
+        int[] scores2 = {20, 50, 50, 53, 80};
+        StudentRecord record2 = new StudentRecord(scores2);
         
+        int[] scores3 = {20, 50, 50, 80};
+        StudentRecord record3 = new StudentRecord(scores3);
+        
+        System.out.println(record1.finalAverage());
+        System.out.println(record2.finalAverage());
+        System.out.println(record3.finalAverage());
     }
 }
