049c2e5cdac5a422bfce39221d27c30dde87aa59
cartras <cjones6096@greenvilleschools.us>
2018-02-07 10:24:55 -0500

    working......

--- 2004/1-WordList/WordListTester.java
+++ 2004/1-WordList/WordListTester.java
@@ -7,27 +7,4 @@
  */
 public class WordListTester
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class WordListTester
-     */
-    public WordListTester()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
