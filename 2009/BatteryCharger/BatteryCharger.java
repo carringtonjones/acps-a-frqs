9daeb49972b2d6e4abf730cc8ea05fceb3f59142
cartras <cjones6096@greenvilleschools.us>
2018-03-02 10:00:14 -0500

    ..

---
+++ 2009/BatteryCharger/BatteryCharger.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class BatteryCharger here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BatteryCharger
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class BatteryCharger
+     */
+    public BatteryCharger()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
