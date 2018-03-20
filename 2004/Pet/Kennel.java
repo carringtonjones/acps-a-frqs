3c0c4da25ac8ba11e72dd3f3ab2b79edbf59618d
cartras <cjones6096@greenvilleschools.us>
2018-03-20 12:06:49 -0400

    updated javadoc

--- 2004/Pet/Kennel.java
+++ 2004/Pet/Kennel.java
@@ -8,7 +8,11 @@ public class Kennel
     {
         this.petList = petList;
     }
-    
+    /** 
+     * postcondition: for each Pet in the kennel, its name followed 
+     * by the result of a call to its speak method has been printed, one line per Pet 
+     * 
+     */
     public void allSpeak()
     {
         for (Pet p : petList) 
