35b428552f21dc04be2fa6733c9cc2d3c3f501ee
cartras <cjones6096@greenvilleschools.us>
2018-03-20 22:14:08 -0400

    done

--- 2004/1-WordList/WordList.java
+++ 2004/1-WordList/WordList.java
@@ -1,10 +1,85 @@
-
 /**
  * 2004 APCS FRQ
  *
  * @author (Carrington Jones)
  * @version (2/5/18)
  */
+import java.util.ArrayList;
+
 public class WordList
 {
+    private ArrayList<String> myList; //contains strings made up of letters
+
+    /**
+     * Constructor for objects of class WordList
+     * 
+     * @param myList list of words
+     */
+    
+    public WordList(ArrayList<String> myList)
+    {
+        this.myList = myList;
+        
+    }
+
+    /**
+     * Counts the number of words with a certain length
+     * @return  count  the word count of the specific length
+     * @param  len  the length of the string thats in the list
+     * 
+     * postcondition: returns the number of words in this WordList that
+     * are exactly len letters long 
+     */
+    
+    public int numWordsOfLength(int len)
+    {
+        
+        int count = 0;
+        
+        for (String x: myList) {
+            
+            if (x.length() == len) count++;
+            
+        }
+        
+        return count;
+        
+    }
+    
+    /**
+     * Removes the all words with a certain length from the list
+     *
+     * @param  len  the length of a string in the list
+     * 
+     * postcondition: all words are exactly len letters long
+     * have been removed from this wordList, with the 
+     * order of the remaining words unchanged
+     */
+    
+    public void removeWordsOfLength(int len) {
+        
+        for (int i = myList.size()-1; i >= 0; i--) {
+            
+            if ((myList.get(i)).length() == len) {
+                
+                myList.remove(i);
+                
+            }
+            
+        }
+    }
+    
+    /**
+     * @return ArraryList as a string
+     */
+    
+    public String toString() {
+        
+         return myList.toString();
+        
+    }
+    
 }
+
+
+
--- 2004/1-WordList/WordListTester.java
+++ 2004/1-WordList/WordListTester.java
@@ -1,10 +1,35 @@
-
 /**
  * Write a description of class WordListTester here.
  *
  * @author (Carrington Jones)
  * @version (2/5/18)
  */
+import java.util.ArrayList;
+
 public class WordListTester
 {
+   public static void main(String[] args)
+   {
+        ArrayList myList = new ArrayList();
+        myList.add("cat");
+        myList.add("mouse");
+        myList.add("frog");
+        myList.add("dog");
+        myList.add("dog");
+        
+        WordList animals = new WordList(myList);
+        
+        System.out.println(animals.numWordsOfLength(4));
+        System.out.println(animals.numWordsOfLength(3));
+        System.out.println(animals.numWordsOfLength(2));
+        
+        animals.removeWordsOfLength(4);
+        System.out.println(animals);
+        
+        animals.removeWordsOfLength(3);
+        System.out.println(animals);
+        
+        animals.removeWordsOfLength(2);
+        System.out.println(animals);
+   }
 }
