b5642bb9d34e2da25ac3b55d06bfbfaffe03d0c6
cartras <cjones6096@greenvilleschools.us>
2018-03-23 10:51:40 -0400

    ...

--- 2005/Ticket/Walkup.java
+++ 2005/Ticket/Walkup.java
@@ -1,10 +1,10 @@
 
 /**
- * Write a description of class Ticket here.
+ * Write a description of class Walkup here.
  *
  * @author (Carrington Jones)
  * @version (3/20/18)
  */
-public class Ticket
+public class Walkup
 {
 }
